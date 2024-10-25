9bdf7e32a71fa9575ba94af636e05958d1864a5d
saif braham <saif.braham@gmail.com>
2024-10-25 21:53:58 +0200

    Calculate total price for unique and complete series purchases

--- DevelopmentBooks/src/main/java/com/kata/developmentbooks/models/Book.java
+++ DevelopmentBooks/src/main/java/com/kata/developmentbooks/models/Book.java
@@ -1,6 +1,39 @@
 package com.kata.developmentbooks.models;
 
 public class Book {
-    public Book(String title, String author, int release) {
+    private String bookTitle;
+    private String bookAuthor;
+    private int publishYear;
+
+    public Book(String bookTitle, String bookAuthor, int publishYear) {
+        this.bookTitle = bookTitle;
+        this.bookAuthor = bookAuthor;
+        this.publishYear = publishYear;
     }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public int getPublishYear() {
+        return publishYear;
+    }
+
+    public void setPublishYear(int publishYear) {
+        this.publishYear = publishYear;
+    }
+
 }
+
