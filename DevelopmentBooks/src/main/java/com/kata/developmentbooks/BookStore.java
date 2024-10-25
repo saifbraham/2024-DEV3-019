eb3f11939e0dc4f93fdb2e15d7c597b2c8c5eb4e
saif braham <saif.braham@gmail.com>
2024-10-25 21:53:58 +0200

    Calculate total price for unique and complete series purchases

--- DevelopmentBooks/src/main/java/com/kata/developmentbooks/BookStore.java
+++ DevelopmentBooks/src/main/java/com/kata/developmentbooks/BookStore.java
@@ -3,12 +3,73 @@ package com.kata.developmentbooks;
 import com.kata.developmentbooks.models.Basket;
 import com.kata.developmentbooks.models.Book;
 
+import java.util.*;
+
 public class BookStore {
 
     private Basket basket = new Basket();
 
     public void addQuantityOfBookToBasket(Book book, int quantity) {
         basket.addToBasket(book, quantity);
+    }
+
+    public boolean isAllQuantityAreEqual(Basket basket) {
+
+        Map<Book, Integer> books = basket.getBasket();
+
+        // Check if the Map is empty or where the first entry is zero
+        if (books.isEmpty() || books.values().iterator().next() == 0) {
+            return false;
+        }
+
+        int firstElement = books.values().iterator().next();
+
+        // Check if all elements match the first element
+        for (Map.Entry<Book, Integer> entry : books.entrySet()) {
+            if (entry.getValue()!= firstElement) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    public double calculateTotalPriceOfAllQuantityAreEqual(Basket basket) {
+
+        int bookPrice = 50;
+        double discount = 0.25;
+        int quantity = basket.getBasket().values().iterator().next();
+        return  quantity * bookPrice * 5 * (1 - discount);
+    }
+
+    public boolean isUniqueBookSeriesPurchased(Basket basket) {
+
+        Map<Book, Integer> books = basket.getBasket();
+        int nonZeroCount = 0;
+
+        for (Integer value : books.values()) {
+            if (value != null && value != 0) {
+                nonZeroCount++;
+                if (nonZeroCount > 1) {
+                    return false; // More than one non-zero value
+                }
+            }
+        }
+
+        // Return true if exactly one non-zero value was found
+        return nonZeroCount == 1;
+    }
+
+    public double calculateTotalPriceOfSingleSeriesPurchase(Basket basket) {
+        Map<Book, Integer> books = basket.getBasket();
+        double totalPrice = 0.0;
+        int bookPrice = 50;
+
+        for (Integer value : books.values()) {
+            totalPrice += value * bookPrice;
+
+        }
+        return totalPrice;
     }
 
     public Basket getBasket() {
@@ -18,5 +79,4 @@ public class BookStore {
     public void setBasket(Basket basket) {
         this.basket = basket;
     }
-
 }
--- DevelopmentBooks/src/test/java/com/kata/developmentbooks/BookStoreTest.java
+++ DevelopmentBooks/src/test/java/com/kata/developmentbooks/BookStoreTest.java
@@ -5,6 +5,7 @@ import com.kata.developmentbooks.models.Book;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BookStoreTest {
 
@@ -36,6 +37,112 @@ class BookStoreTest {
         assertEquals(1, myBasket.getBasket().getOrDefault(book5,0));
     }
 
+    @Test
+    @DisplayName("Verify if the quantities are equal across each book series")
+    void testIfAllQuantityOfBooksAreEqual(){
+
+        // Arrange: Set up a store and create a book basket with specified purchase quantities
+        BookStore store = new BookStore();
+
+        Book book1 = new Book("Clean Code", "Robert Martin", 2008);
+        Book book2 = new Book("The Clean Coder", "Robert Martin", 2011);
+        Book book3 = new Book("Clean Architecture", "Robert Martin", 2017);
+        Book book4 = new Book("Test Driven Development by Example", "Kint Beck", 2003);
+        Book book5 = new Book("Working Effectively With Legacy Code", "Michael C. Feathers", 2004);
+        store.addQuantityOfBookToBasket(book1,2);
+        store.addQuantityOfBookToBasket(book2,2);
+        store.addQuantityOfBookToBasket(book3,2);
+        store.addQuantityOfBookToBasket(book4,2);
+        store.addQuantityOfBookToBasket(book5,2);
+
+
+
+        // Act: Ensure each book quantity is identical
+        boolean isQuantityEqual = store.isAllQuantityAreEqual(store.getBasket());
+
+        // Assert: Confirm that each book has the same quantity
+        assertTrue(isQuantityEqual);
+
+    }
+
+    @Test
+    @DisplayName("Compute the total price for equal quantities of book series, applying a 25% discount")
+    void testCalculateTotalPriceOfAllQuantityAreEqual(){
+
+        // Arrange: Set up a store and create a book basket with specified purchase quantities
+        BookStore store = new BookStore();
+
+        Book book1 = new Book("Clean Code", "Robert Martin", 2008);
+        Book book2 = new Book("The Clean Coder", "Robert Martin", 2011);
+        Book book3 = new Book("Clean Architecture", "Robert Martin", 2017);
+        Book book4 = new Book("Test Driven Development by Example", "Kint Beck", 2003);
+        Book book5 = new Book("Working Effectively With Legacy Code", "Michael C. Feathers", 2004);
+        store.addQuantityOfBookToBasket(book1,2);
+        store.addQuantityOfBookToBasket(book2,2);
+        store.addQuantityOfBookToBasket(book3,2);
+        store.addQuantityOfBookToBasket(book4,2);
+        store.addQuantityOfBookToBasket(book5,2);
+
+        // Act: Calculate total price with 25% of discount
+        double price = store.calculateTotalPriceOfAllQuantityAreEqual(store.getBasket());
+
+        // Assert: Check that the total price matches the expected value
+        assertEquals(375, price);
+
+    }
+
+
+    @Test
+    @DisplayName("Check if only a single book series is being purchased")
+    void testIfSingleBookIsPurchased() {
+
+        // Arrange: Set up a store and create a book basket with specified purchase quantities
+        BookStore store = new BookStore();
+
+        Book book1 = new Book("Clean Code", "Robert Martin", 2008);
+        Book book2 = new Book("The Clean Coder", "Robert Martin", 2011);
+        Book book3 = new Book("Clean Architecture", "Robert Martin", 2017);
+        Book book4 = new Book("Test Driven Development by Example", "Kint Beck", 2003);
+        Book book5 = new Book("Working Effectively With Legacy Code", "Michael C. Feathers", 2004);
+        store.addQuantityOfBookToBasket(book1, 0);
+        store.addQuantityOfBookToBasket(book2, 0);
+        store.addQuantityOfBookToBasket(book3, 0);
+        store.addQuantityOfBookToBasket(book4, 0);
+        store.addQuantityOfBookToBasket(book5, 2);
+
+        // Act: Ensure that only one book series is purchased
+        boolean isUniqueBookSeriesPurchased = store.isUniqueBookSeriesPurchased(store.getBasket());
+
+        // Assert: Confirm that one book series is purchased
+        assertTrue(isUniqueBookSeriesPurchased);
+
+    }
+
+    @Test
+    @DisplayName("Calculate total price without discount for a single book series purchase")
+    void testCalculateTotalPriceSingleSeriesPurchase() {
+
+        // Arrange: Set up a store and create a book basket with specified purchase quantities
+        BookStore store = new BookStore();
+
+        Book book1 = new Book("Clean Code", "Robert Martin", 2008);
+        Book book2 = new Book("The Clean Coder", "Robert Martin", 2011);
+        Book book3 = new Book("Clean Architecture", "Robert Martin", 2017);
+        Book book4 = new Book("Test Driven Development by Example", "Kint Beck", 2003);
+        Book book5 = new Book("Working Effectively With Legacy Code", "Michael C. Feathers", 2004);
+        store.addQuantityOfBookToBasket(book1,0);
+        store.addQuantityOfBookToBasket(book2,0);
+        store.addQuantityOfBookToBasket(book3,3);
+        store.addQuantityOfBookToBasket(book4,0);
+        store.addQuantityOfBookToBasket(book5,0);
+
+        // Act: Calculate total price with 0% of discount
+        double price = store.calculateTotalPriceOfSingleSeriesPurchase(store.getBasket());
+
+        // Assert: Check that the total price matches the expected value
+        assertEquals(150, price);
+
+    }
 
 
 }
